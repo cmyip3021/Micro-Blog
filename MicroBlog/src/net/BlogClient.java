3b7df70841ad8d3505046c3180789c59d099ba37
cmyipaa <cmyipaa@CSL1WK06.win2k.cs.ust.hk>
2015-04-28 16:07:15 +0800

    Q on t2

---
+++ MicroBlog/src/net/BlogClient.java
@@ -0,0 +1,41 @@
+package net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class BlogClient {
+
+	public static final String IP="127.0.0.1";
+	public static final int PORT=3021;
+
+	public static void main (String[] args){
+		try{
+			Socket socket = new Socket(IP,PORT);
+			PrintWriter out=
+					new PrintWriter(socket.getOutputStream(),true);
+			BufferedReader in =
+					new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out.println("post?");//send to server
+
+			String userInput;
+			while((userInput=in.readLine())!=null){
+
+				System.out.println( userInput);
+			}
+			socket.close();
+			in.close();
+			out.close();
+		}catch(UnknownHostException e){
+			System.err.println("don't know about host"+IP);
+			System.exit(1);
+		}catch(IOException e){
+			System.err.println("couldn't get I/O for the connection to"+IP);
+			System.exit(1);
+		}
+	}
+
+}
